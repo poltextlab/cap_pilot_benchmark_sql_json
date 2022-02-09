7b7cc1c5863e512f950e21c019f64c198a1a468d
uveges <47694417+uveges@users.noreply.github.com>
2022-02-09 09:54:05 +0100

    Add files via upload

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 # Please set to following:
 
-HOST=""
-USER=""
-PASSWORD=""
-DATABASE = ""
+HOST="localhost"
+USER="root"
+PASSWORD="Jelszo89"
+DATABASE = "test"
+
+# Only relevant, if JSON generation mode is ON (main.py, at line 21.: 'CREATE_JSON_AFTER_SQL' variable is set to True)
+OUTPUT_JSON = "resoures/tk_parlament.json"
 
 #Do not modifiy at the beginning!
 SENTENCE_ID = 0
